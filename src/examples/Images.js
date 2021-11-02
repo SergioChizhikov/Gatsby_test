39cbb6f486963435b18a62e331d6a0dae4e3f660
Sergey Chizhikov <chchizh@gmail.com>
2021-11-02 12:00:56 +0300

    working with static image

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -1,19 +1,38 @@
 import React from "react"
 import styled from "styled-components"
-//import big from "../assets/images/big.jpg"
 import { StaticImage } from "gatsby-plugin-image"
 
 const Images = () => {
   return (
     <Wrapper>
-      <h2>Gatsby Img</h2>
-      <StaticImage src="../assets/images/big.jpg" alt="food"/>
+      <article>
+        <h4>constrained / default</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          className="example-img"
+          placeholder="tracedSVG"
+          layout="constrained"
+          as="section"
+        />
+      </article>
     </Wrapper>
   )
 }
 const Wrapper = styled.section`
-  img {
-    width: 200px;
+  width: 70vw;
+  margin: 0 auto;
+  display: grid;
+  text-align: center;
+  gap: 2rem;
+  article {
+    border: 2px solid red;
+  }
+  .example-img {
+    border-radius: 1rem;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr 1fr;
   }
 `
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,30 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const About = () => {
   return (
     <Layout>
-      <h1>About page</h1>
-      <h1>Hello</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>I'm baby coloring book poke taxidermy</h2>
+            <p>
+              Taxidermy forage glossier letterpress heirloom before they sold
+              out you probably haven't heard of them banh mi biodiesel chia.
+            </p>
+            <p>
+              Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
+              retro.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage src="../assets/images/about.jpeg" alt="Person pouring salt"/>
+        </section>
+      </main>
     </Layout>
   )
 }
